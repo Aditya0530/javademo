df19d4caba413983b173a806805524292afdbf30
aditya <aditya@nameisadi>
2025-01-23 15:55:51 +0530

    Format Class

--- GitHubDemo/src/com/adi/Test.java
+++ GitHubDemo/src/com/adi/Test.java
@@ -1,14 +1,16 @@
 package com.adi;
 
 public class Test {
-public void m1() {
-	System.out.println("m1");
-}
-public void m2() {
-	System.out.println("m2");
-}
+	public void m1() {
+		System.out.println("m1");
+	}
+
+	public void m2() {
+		System.out.println("m2");
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hello");
 
 	}
