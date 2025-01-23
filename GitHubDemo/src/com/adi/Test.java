03471d0855fbacc377e1e5f5ea7df595ca95fc07
Aditya0530 <adityaasonawane121@gmail.com>
2025-01-24 01:11:26 +0530

    Update Test.java

--- GitHubDemo/src/com/adi/Test.java
+++ GitHubDemo/src/com/adi/Test.java
@@ -2,16 +2,16 @@ package com.adi;
 
 public class Test {
 	public void m1() {
-		System.out.println("m1");
+		System.out.println("m1-method");
 	}
 
 	public void m2() {
-		System.out.println("m2");
+		System.out.println("m2-method");
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello");
+		System.out.println("Hello Main Method");
 
 		Test t = new Test();
 		t.m1();
