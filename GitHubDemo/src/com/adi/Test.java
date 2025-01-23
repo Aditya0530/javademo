f5ccc6d90090a729874c08f951b8f06dea729302
aditya <aditya@nameisadi>
2025-01-24 01:13:18 +0530

    Updated

--- GitHubDemo/src/com/adi/Test.java
+++ GitHubDemo/src/com/adi/Test.java
@@ -1,22 +1,21 @@
 package com.adi;
 
-public class Test {
-	public void m1() {
+public class Test 
+{
+	public void m1() 
+	{
 		System.out.println("m1-method");
 	}
-
-	public void m2() {
+	public void m2() 
+	{
 		System.out.println("m2-method");
 	}
-
 	public static void main(String[] args) {
 
 		System.out.println("Hello Main Method");
-
 		Test t = new Test();
 		t.m1();
 		t.m2();
-
 	}
 
 }
