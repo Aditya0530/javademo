2ead6e2a2fdbf7b9cf5c179f423880d78cabe74c
Aditya0530 <adityaasonawane121@gmail.com>
2025-01-23 16:01:07 +0530

    Update Test.java

--- GitHubDemo/src/com/adi/Test.java
+++ GitHubDemo/src/com/adi/Test.java
@@ -5,10 +5,6 @@ public class Test {
 		System.out.println("m1");
 	}
 
-	public void m2() {
-		System.out.println("m2");
-	}
-
 	public static void main(String[] args) {
 
 		System.out.println("Hello");
