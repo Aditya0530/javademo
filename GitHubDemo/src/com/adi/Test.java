a168ab3c6303b2ef2fcaebedf578ff0f883deee0
aditya <aditya@nameisadi>
2025-01-24 12:06:00 +0530

    Added one method

--- GitHubDemo/src/com/adi/Test.java
+++ GitHubDemo/src/com/adi/Test.java
@@ -10,12 +10,17 @@ public class Test
 	{
 		System.out.println("m2-method");
 	}
+	public void m3() 
+	{
+		System.out.println("m3-method");
+	}
 	public static void main(String[] args) {
 
 		System.out.println("Hello Main Method");
 		Test t = new Test();
 		t.m1();
 		t.m2();
+		t.m3();
 	}
 
 }
