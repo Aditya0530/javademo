b3df84c6f89a17806fc1b19376f54fde527e2df1
Aditya0530 <adityaasonawane121@gmail.com>
2025-01-24 15:14:32 +0530

    add m5 method

--- GitHubDemo/src/com/adi/Test.java
+++ GitHubDemo/src/com/adi/Test.java
@@ -4,18 +4,14 @@ public class Test {
 	public void m1() {
 		System.out.println("m1-method");
 	}
-
 	public void m2() {
 		System.out.println("m2-method");
 	}
-
 	public void m3() {
 		System.out.println("m3-method");
 	}
-
 	public void m5() {
 		System.out.println("m5-method");
-
 	}
 
 	public static void main(String[] args) {
