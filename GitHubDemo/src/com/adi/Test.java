5af9a27545a8ed54580ce0820691ef52cc2ef941
Aditya0530 <adityaasonawane121@gmail.com>
2025-01-24 12:04:56 +0530

    add m4 method

--- GitHubDemo/src/com/adi/Test.java
+++ GitHubDemo/src/com/adi/Test.java
@@ -10,12 +10,17 @@ public class Test
 	{
 		System.out.println("m2-method");
 	}
+	public void m4() 
+	{
+		System.out.println("m4-method");
+	}
 	public static void main(String[] args) {
 
 		System.out.println("Hello Main Method");
 		Test t = new Test();
 		t.m1();
 		t.m2();
+		t.m4();
 	}
 
 }
