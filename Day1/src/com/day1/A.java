cc98be216742584b5416ab04326e3daf18163dd4
aditya <aditya@nameisadi>
2025-01-27 00:38:42 +0530

    Add m1 from dev1

---
+++ Day1/src/com/day1/A.java
@@ -0,0 +1,13 @@
+package com.day1;
+
+public class A {
+
+	public void m1() {
+		
+	}
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+		System.out.println("World");
+	}
+}
---
+++ Day1/src/com/day1/B.java
@@ -0,0 +1,11 @@
+package com.day1;
+
+public class B {
+
+	public static void main(String args[]) {
+		System.out.println("Myself:" + "Aditya Sonawane");
+		System.out.println("Im From:" + "Shahada");
+
+	}
+
+}
